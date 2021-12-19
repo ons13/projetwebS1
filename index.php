cd04bc666138bcd02d059bd01589759be1590c50
ons13 <Onsbhs3@gmail.com>
2021-12-19 19:59:32 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+?>
+<html>
+    <head>
+        <title> my website </title>
+    </head>
+<body>
+    <a href="logout.php">logout</a>
+    <h1> this is the index page</h1>
+
+    <br>
+    Hey,Username.
+</body>
+</html>
